00ec0c171df046a3be07a11f364854bfd6878e5c
Paul Kasidit <paulkasidit@gmail.com>
2022-07-02 13:57:05 -0700

    cleaned up project

--- js/scripts.js
+++ js/scripts.js
@@ -18,28 +18,22 @@ function generateRobogerLanguage(e){
   const userInput = parseInt(document.getElementById("userInput").value);
   let processingArray = createUserInputArray(userInput).map(String); //user array is converted into an array of strings for processing 
 
-  console.log(processingArray); 
-
   resultArray = [] //push the results from the processingArray into a resultsList, to be converted into a string later
 
   processingArray.forEach((num) => {
-    if ((num).match('1')){
-      resultArray.push("Beep!");
+    if ((num).match('1')){ //using array matching expressions to find strings containing each number, this method does not work properly with an array of numbers
+      resultArray.push("Beep!"); //strings that meet this condition are pushed to the result array 
     } else if ((num).match('2')){
       resultArray.push("Boop");
     } else if ((num).match('3')){
       resultArray.push("Won\'t you be my neighbor?"); 
     } else {
-      resultArray.push(num);
+      resultArray.push(num); //strings that meet no conditions are left as is and pushed to the result array
     }
   });
 
-  let result =  resultArray.toString().replaceAll(',', ' ');   
+  let result =  resultArray.toString().replaceAll(',', ' '); //convert array to string, each seperated by a space   
   document.getElementById("result").innerText = result; 
-
-
-  console.log(result);
-  console.log(typeof(result));
 
   let resultsArea = document.getElementById("results-area"); 
   resultsArea.removeAttribute("style");
