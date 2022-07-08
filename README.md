63f87d84159d2f3afb2f4491a45a89b745cc1936
Paul Kasidit <paulkasidit@gmail.com>
2022-07-08 13:15:23 -0700

    fixed code and readme

--- README.md
+++ README.md
@@ -12,7 +12,7 @@
 
 ## Tests 
 
-Tess for the project can be viewed [here]('tests.txt')
+Tess for the project can be viewed [here](tests.txt)
 
 ## Description
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,23 @@
+
 //Business Logic
-
-function createUserInputArray(num) {  //this callback function takes in the user's input and outputs an array from 0 to the user's number 
+  //function to create an array from user input  
+  //callback function to take in the user's input and outputs an array from 0 to the user's number 
+function createUserInputArray(num) {  
   
-  let userInputArray = []; 
-
-  for(let i = 0; i <= num; i++){
-    userInputArray.push(i); 
-  };
+    let userInputArray = []; 
+      for(let i = 0; i <= num; i++){
+        userInputArray.push(i); 
+      };
+    return userInputArray; 
   
-  return userInputArray; 
 } 
 
-function generateRobogerLanguage(e){ 
-
-  e.preventDefault(); 
-
-  const userInput = parseInt(document.getElementById("userInput").value);
-  let processingArray = createUserInputArray(userInput).map(String); //user array is converted into an array of strings for processing 
+function generateRobogerLanguage(arr) { 
 
   resultArray = [] //push the results from the processingArray into a resultsList, to be converted into a string later
 
-  processingArray.forEach((num) => {
+
+  arr.forEach((num) => {
     if ((num).match('3')){ //using array matching expressions to find strings containing each number, this method does not work properly with an array of numbers
       resultArray.push("Won\'t you be my neighbor?"); //strings that meet this condition are pushed to the result array 
     } else if ((num).match('2')){
@@ -32,17 +29,28 @@ function generateRobogerLanguage(e){
     }
   });
 
-  let result =  resultArray.toString().replaceAll(',', ' '); //convert array to string, each seperated by a space   
-  document.getElementById("result").innerText = result; 
+  return resultArray;
+}
 
+//User Logic 
+  //function to output results to DOM
+function outputResult(e){ 
+
+  let userInput = parseInt(document.getElementById("userInput").value);
+  let processingArray = createUserInputArray(userInput).map(String); 
+
+  e.preventDefault(); 
+
+  const result =  generateRobogerLanguage(processingArray).toString().replaceAll(',', ' '); //convert array to string, each seperated by a space   
+      //hiding and then unhiding result
+
+  document.getElementById("result").innerText = result; 
   let resultsArea = document.getElementById("results-area"); 
   resultsArea.removeAttribute("style");
 
-  return result
-}; 
+  return result; //the final result is returned
+}
 
-
-//User Logic 
 window.addEventListener("load", function(){
   
   event.preventDefault();
@@ -51,6 +59,6 @@ window.addEventListener("load", function(){
   resultsArea.style.display = "none";
 
   const form  = document.getElementById("translationForm"); 
-  form.addEventListener("submit",generateRobogerLanguage);
+  form.addEventListener("submit",outputResult);
 
 });
